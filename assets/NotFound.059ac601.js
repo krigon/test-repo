dea46a8202d708c3d2c524d3751a3b10c4e45ce9
krigon <42438173+krigon@users.noreply.github.com>
2022-07-27 04:26:26 +0900

    Add files via upload

---
+++ assets/NotFound.059ac601.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as s,p as _,a as c,b as a}from"./index.a8d3fb6c.js";const d={},n=e=>(_("data-v-2e4be153"),e=e(),c(),e),r={class:"tc"},p=n(()=>a("div",{class:"title"},"404 Not Found",-1)),i=[p];function u(e,l){return t(),s("div",r,i)}var f=o(d,[["render",u],["__scopeId","data-v-2e4be153"]]);export{f as default};
